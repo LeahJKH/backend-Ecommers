aa19a469f733ba05d7e424a5c07517343d8cb2ff
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-12-06 10:01:53 +0100

    fix user/username bug

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,28 +2,27 @@ const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-  //const { user, pwd } = req.body; //destructuring didn't want to work
-  const user = req.body.username;
-  const pwd = req.body.password;
-  if (!user || !pwd)
+  const { username, password } = req.body;
+
+  if (!username || !password)
     //if the request is missing either a username or a password, send error, both are required
     return res.status(400).json({ message: "Username and password are required" });
 
   //check for a duplicate username, send conflict status if duplicate found
-  const duplicate = await User.findOne({ username: user }).exec();
+  const duplicate = await User.findOne({ username: username }).exec();
   if (duplicate) {
     return res.sendStatus(409);
   }
   try {
     //hash and salt the password to encrypt it so not even the database admin can look up user passwords in the database
-    const hashedPwd = await bcrypt.hash(pwd, 13);
+    const hashedPassword = await bcrypt.hash(password, 13);
     const result = await User.create({
-      username: user,
-      password: hashedPwd,
+      username: username,
+      password: hashedPassword,
     });
     console.log(result);
     //confirm new user has been created
-    res.status(201).json({ success: `New user ${user} has been created!` });
+    res.status(201).json({ success: `New user ${username} has been created!` });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
